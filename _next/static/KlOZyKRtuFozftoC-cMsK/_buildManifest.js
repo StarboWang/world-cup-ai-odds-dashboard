7999f4e57333ada9f37e4066a0939a48aed600af
王博 <bo@wangbodeMacBook-Pro.local>
2026-06-14 21:03:46 +0800

    Publish static site

---
+++ _next/static/KlOZyKRtuFozftoC-cMsK/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/world-cup-ai-odds-dashboard//_next/:path+",
+        "destination": "/world-cup-ai-odds-dashboard/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
